8f7004942a3e55905268743962d3ff92066dbf9a
= <tlam036@ucr.edu>
2020-08-12 23:27:30 -0700

    Initial commit for lab 8

--- source/main.c
+++ source/main.c
@@ -12,54 +12,106 @@
 #include "simAVRHeader.h"
 #endif
 
-void ADC_init(){
-	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+void set_PWM(double frequency){
+	static double current_frequency;
+
+	if(frequency != current_frequency){
+		if(!frequency) { TCCR3B &= 0x08; }
+		else { TCCR3B |= 0x03; }
+
+		if (frequency < 0.954) { OCR3A = 0xFFFF; }
+		else if (frequency > 31250) { OCR3A = 0x0000; }
+		else { OCR3A = (short)(8000000 / (128 * frequency)) - 1; }
+
+		TCNT3 = 0;
+		current_frequency = frequency;
+	}
+}
+
+void PWM_on() {
+	TCCR3A = (1 << COM3A0);
+
+	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+
+	set_PWM(0);
+}
+
+void PWM_off() {
+	TCCR3A = 0x00;
+	TCCR3B = 0x00;
+}
+
+enum States {start, init, playC, playD, playE} state;
+
+void tick() {
+	unsigned char button = ~PINA & 0x07;
+
+	switch(state){
+		case start:
+			state = init;
+			break;
+		case init:
+			if(button == 0x01){
+				state = playC;
+			}
+			else if(button == 0x02){
+				state = playD;
+			}
+			else if(button == 0x04){
+				state = playE;
+			}
+			else{
+				state = init;
+			}
+			break;
+		case playC:
+			state = (button == 0x01) ? playC : init;
+			break;
+		case playD:
+                        state = (button == 0x02) ? playD : init;
+                        break;
+		case playE:
+                        state = (button == 0x04) ? playE : init;
+                        break;
+		default:
+			state = start;
+			break;
+	}
+
+	switch(state){
+		case init:
+			set_PWM(0);
+			break;
+		case playC:
+			set_PWM(261.63);
+			break;
+		case playD:
+			set_PWM(293.66);
+			break;
+		case playE:
+			set_PWM(329.63);
+			break;
+		default:
+			break;
+	}
 }
 
 int main(void) {
 
-	ADC_init();
-
     /* Insert DDR and PORT initializations */
-	DDRB = 0xFF; PORTB = 0x00;
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x40; PORTB = 0x00;
     /* Insert your solution below */
-	unsigned char led = 0;
-	unsigned short max = 0x00C9;
+	
+	PWM_on();	
+	
+	state = start;
+
 	while(1){
-		unsigned short x = ADC;
-
-		if(x >= 200){
-			led = 0xFF;
-		}
-		else if(x < 200 && x >= 175){
-			led = 0x7F;
-		}
-		else if(x < 175 && x >= 150){
-			led = 0x3F;
-		}
-		else if(x < 150 && x >= 125){
-			led = 0x1F;
-		}
-		else if(x < 125 && x >= 100){
-			led = 0x0F;
-		}
-		else if(x < 100 && x >= 75){
-			led = 0x07;
-		}
-		else if(x < 75 && x >= 50){
-			led = 0x03;
-		}
-		else if(x < 50 && x >= 25){
-			led = 0x01;
-		}
-		else{
-			led = 0;
-		}
-
-		PORTB = led;
-
+	
+		tick();
 
 	}
-
+    		
     return 1;
 }
